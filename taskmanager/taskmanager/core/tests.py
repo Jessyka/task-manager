2ed2ee56fb52d72b5ceb672e03b8104003f7e5e1
Jessyka Lage <jessykalage@gmail.com>
2020-01-11 18:11:57 -0300

    Implements core tests

--- taskmanager/taskmanager/core/tests.py
+++ taskmanager/taskmanager/core/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.test.client import Client
 
-# Create your tests here.
+class HomeViewTest(TestCase):
+    def test_home_status_code(self):
+        cliente = Client()
+        response = cliente.get('/home/')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_home_template_used(self):
+        cliente = Client()
+        response = cliente.get('/home/')
+        self.assertTemplateUsed(response, 'home.html')
