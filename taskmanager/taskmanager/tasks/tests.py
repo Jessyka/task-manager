f1e8692937b9ef0b67d7ebe526b00cd35aa75f77
Jessyka Lage <jessykalage@gmail.com>
2020-01-11 18:12:49 -0300

    Implements task model tests

--- taskmanager/taskmanager/tasks/tests.py
+++ taskmanager/taskmanager/tasks/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Task
+from django.test.client import Client 
 
-# Create your tests here.
+class TaskTestCase(TestCase):
+    
+    def setUp(self):
+        self.task = Task.objects.create(description = 'My_teste', status='new')
+
+    def tearDown(self):
+        self.task.delete()
+
+    def test_task_form_success(self):
+        data = {'description': 'my new description'}
+        path = '/newtask/'
+        client = Client()
+        
+        response = client.post(path, data)
+        self.assertEqual(response.status_code, 200)
